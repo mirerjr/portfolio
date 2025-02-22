135226bbd67b79477c7a4cede751115488bd58fb
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2025-02-22 10:32:26 -0300

    refactor: simplify titles and section structures

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
-<script setup></script>
+<script setup>
+import BaseSection from "./components/BaseSection.vue";
+</script>
 
 <template>
-  <header>
-    <h1>Mirer Balbino</h1>
-    <h2>de Andrade Junior</h2>
-    <h3>[] Desenvolvedor Full stack</h3>
+  <header class="flex flex-col gap-6">
+    <div>
+      <h1 class="font-bold">
+        Mirer Balbino
+        <span class="block font-normal leading-1">
+          de Andrade Junior
+        </span>
+      </h1>
+    </div>
+    <p class="text-xl">[] Desenvolvedor Full stack</p>
   </header>
   <main>
-    <section>
-      <article>
-        <h4>Sobre mim</h4>
+    <section class="flex">
+      <BaseSection title="Sobre mim">
         <p>
           Experiência no desenvolvimento de aplicações REST usando Java (Grails,
           Spring) e JavaScript (Vue.js). Proativo e metódico, busco entregar
           soluções que otimizem processos e melhorem a experiência do usuário.
         </p>
-      </article>
-      <article>
-        <h4>Contatos</h4>
+      </BaseSection>
+      <BaseSection title="Contatos">
         <ul>
           <li>
             []
-            <a href="https://github.com/mirerjr"> @mirerjr </a>
+            <a href="https://github.com/mirerjr">@mirerjr</a>
           </li>
           <li>
             []
@@ -36,96 +42,110 @@
             </a>
           </li>
         </ul>
-      </article>
+      </BaseSection>
     </section>
-    <section>
-      <h4>Meus projetos</h4>
-      <article>
-        <header>
-          <h5>Transportae</h5>
-          <span>[]</span>
-        </header>
+    <BaseSection title="Meus projetos">
+      <div class="flex">
         <article>
+          <header>
+            <h3>Transportae</h3>
+            <span>[]</span>
+          </header>
           <p>
             Sistema para gerenciamento e acompanhamento das linhas de transporte
             escolar, para uso pela prefeitura e pelos estudantes
           </p>
         </article>
-      </article>
-      <article>
-        <header>
-          <h5>Support Tickets</h5>
-          <span>[]</span>
-        </header>
         <article>
+          <header>
+            <h3>Support Tickets</h3>
+            <span>[]</span>
+          </header>
           <p>Gestão de tickets de suporte que...</p>
         </article>
-      </article>
-      <article>
-        <header>
-          <h5>GuardMyPdf</h5>
-          <span>[]</span>
-        </header>
         <article>
+          <header>
+            <h3>GuardMyPdf</h3>
+            <span>[]</span>
+          </header>
           <p>Aplicação que adiciona senha aos PDFs...</p>
         </article>
-      </article>
-    </section>
-    <section>
-      <h4>Experiências</h4>
-      <article>
-        <h6>Procuradoria Geral do Município de Aracaju (PGMA)</h6>
-        <ul>
-          <li>
-            []
-            <div>
-              <span>06/2023 - Hoje</span>
-              <p>Analista de Sistemas Pleno (SergipeTec)</p>
-            </div>
-          </li>
-          <li>
-            []
-            <div>
-              <span>12/2020 - 06/2023</span>
-              <p>Desenvolvedor (PGMA)</p>
-            </div>
-          </li>
-        </ul>
-      </article>
-      <article>
-        <h6>Instituto Federal de Sergipe (IFS)</h6>
-        <ul>
-          <li>
-            []
-            <div>
-              <span>2019</span>
-              <p>Trainee - suporte</p>
-            </div>
-          </li>
-        </ul>
-      </article>
-    </section>
-    <section>
-      <h4>Escolaridade</h4>
-      <article>
-        <h6>Instituto Federal de Sergipe (IFS)</h6>
-        <ul>
-          <li>
-            []
-            <div>
-              <span>2020-2023</span>
-              <p>Tecnólogo em Análise e Desenvolvimento de sistemas</p>
-            </div>
-          </li>
-          <li>
-            []
-            <div>
-              <span>2017-2019</span>
-              <p>Técnico em Informática</p>
-            </div>
-          </li>
-        </ul>
-      </article>
-    </section>
+      </div>
+    </BaseSection>
+    <div class="flex">
+      <BaseSection title="Experiências">
+        <article>
+          <h4>Procuradoria Geral do Município de Aracaju (PGMA)</h4>
+          <ul>
+            <li>
+              []
+              <div>
+                <span>06/2023 - Hoje</span>
+                <p>Analista de Sistemas Pleno (SergipeTec)</p>
+              </div>
+            </li>
+            <li>
+              []
+              <div>
+                <span>12/2020 - 06/2023</span>
+                <p>Desenvolvedor (PGMA)</p>
+              </div>
+            </li>
+          </ul>
+        </article>
+        <article>
+          <h4>Instituto Federal de Sergipe (IFS)</h4>
+          <ul>
+            <li>
+              []
+              <div>
+                <span>2019</span>
+                <p>Trainee - suporte</p>
+              </div>
+            </li>
+          </ul>
+        </article>
+      </BaseSection>
+      <BaseSection title="Escolaridade">
+        <article>
+          <h4>Instituto Federal de Sergipe (IFS)</h4>
+          <ul>
+            <li>
+              []
+              <div>
+                <span>2020-2023</span>
+                <p>Tecnólogo em Análise e Desenvolvimento de sistemas</p>
+              </div>
+            </li>
+            <li>
+              []
+              <div>
+                <span>2017-2019</span>
+                <p>Técnico em Informática</p>
+              </div>
+            </li>
+          </ul>
+        </article>
+      </BaseSection>
+    </div>
   </main>
 </template>
+
+<style>
+@layer base {
+  h1 {
+    font-size: var(--text-3xl);
+  }
+  h2 {
+    font-size: var(--text-xl);
+    font-weight: bold;
+  }
+  h3 {
+    font-size: var(--text-xl);
+    font-weight: 600;
+  }
+  h4 {
+    font-size: var(--text-lg);  
+  }
+}
+</style>
---
+++ src/components/BaseSection.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <section class="flex flex-col gap-4">
+    <h2 class="text-xl font-bold">{{ title }}</h2>
+    <slot></slot>
+  </section>
+</template>
