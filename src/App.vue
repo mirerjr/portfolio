245d97ea921e48c74ff8d71629c3c53c8c852fa6
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2025-02-22 09:17:03 -0300

    feat: initial HTML structure with main content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,131 @@
 <script setup></script>
 
 <template>
+  <header>
+    <h1>Mirer Balbino</h1>
+    <h2>de Andrade Junior</h2>
+    <h3>[] Desenvolvedor Full stack</h3>
+  </header>
   <main>
-    Mirer Balbino de Andrade Junior
+    <section>
+      <article>
+        <h4>Sobre mim</h4>
+        <p>
+          Experiência no desenvolvimento de aplicações REST usando Java (Grails,
+          Spring) e JavaScript (Vue.js). Proativo e metódico, busco entregar
+          soluções que otimizem processos e melhorem a experiência do usuário.
+        </p>
+      </article>
+      <article>
+        <h4>Contatos</h4>
+        <ul>
+          <li>
+            []
+            <a href="https://github.com/mirerjr"> @mirerjr </a>
+          </li>
+          <li>
+            []
+            <a href="https://www.linkedin.com/in/mirer-balbino">
+              mirer-balbino
+            </a>
+          </li>
+          <li>
+            []
+            <a href="mailto:mirer.balbino@gmail.com">
+              mirer.balbino@gmail.com
+            </a>
+          </li>
+        </ul>
+      </article>
+    </section>
+    <section>
+      <h4>Meus projetos</h4>
+      <article>
+        <header>
+          <h5>Transportae</h5>
+          <span>[]</span>
+        </header>
+        <article>
+          <p>
+            Sistema para gerenciamento e acompanhamento das linhas de transporte
+            escolar, para uso pela prefeitura e pelos estudantes
+          </p>
+        </article>
+      </article>
+      <article>
+        <header>
+          <h5>Support Tickets</h5>
+          <span>[]</span>
+        </header>
+        <article>
+          <p>Gestão de tickets de suporte que...</p>
+        </article>
+      </article>
+      <article>
+        <header>
+          <h5>GuardMyPdf</h5>
+          <span>[]</span>
+        </header>
+        <article>
+          <p>Aplicação que adiciona senha aos PDFs...</p>
+        </article>
+      </article>
+    </section>
+    <section>
+      <h4>Experiências</h4>
+      <article>
+        <h6>Procuradoria Geral do Município de Aracaju (PGMA)</h6>
+        <ul>
+          <li>
+            []
+            <div>
+              <span>06/2023 - Hoje</span>
+              <p>Analista de Sistemas Pleno (SergipeTec)</p>
+            </div>
+          </li>
+          <li>
+            []
+            <div>
+              <span>12/2020 - 06/2023</span>
+              <p>Desenvolvedor (PGMA)</p>
+            </div>
+          </li>
+        </ul>
+      </article>
+      <article>
+        <h6>Instituto Federal de Sergipe (IFS)</h6>
+        <ul>
+          <li>
+            []
+            <div>
+              <span>2019</span>
+              <p>Trainee - suporte</p>
+            </div>
+          </li>
+        </ul>
+      </article>
+    </section>
+    <section>
+      <h4>Escolaridade</h4>
+      <article>
+        <h6>Instituto Federal de Sergipe (IFS)</h6>
+        <ul>
+          <li>
+            []
+            <div>
+              <span>2020-2023</span>
+              <p>Tecnólogo em Análise e Desenvolvimento de sistemas</p>
+            </div>
+          </li>
+          <li>
+            []
+            <div>
+              <span>2017-2019</span>
+              <p>Técnico em Informática</p>
+            </div>
+          </li>
+        </ul>
+      </article>
+    </section>
   </main>
 </template>
