d851e307724ced915ec721ce86765f72c11b1f81
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2025-02-24 22:56:56 -0300

    feat: add link target and hover style

--- src/components/PersonalLink.vue
+++ src/components/PersonalLink.vue
@@ -17,7 +17,12 @@ defineProps({
 
 <template>
   <li>
-    <a :href="link" class="flex items-center gap-2">
+    <a
+      :href="link"
+      target="_blank"
+      rel="noopener"
+      class="relative box-border flex w-fit items-center gap-2 after:absolute after:bottom-0 after:left-0.5 after:w-0 after:border-b-2 after:border-main after:duration-300 after:ease-out hover:after:w-full"
+    >
       <fontAwesomeIcon :icon="icon" class="w-5" />
       {{ label }}
     </a>
