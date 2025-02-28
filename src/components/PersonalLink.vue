21e7bfef70c6439157a812ff181eea9a21ab8089
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2025-02-27 21:59:28 -0300

    style: custom border-underline effect for PersonalLink

--- src/components/PersonalLink.vue
+++ src/components/PersonalLink.vue
@@ -21,10 +21,19 @@ defineProps({
       :href="link"
       target="_blank"
       rel="noopener"
-      class="relative box-border flex w-fit items-center gap-2 after:absolute after:bottom-0 after:left-0.5 after:w-0 after:border-b-2 after:border-main after:duration-300 after:ease-out hover:after:w-full"
+      class="link-border-underline relative box-border flex w-fit items-center gap-x-2 rounded-sm pr-0.5"
     >
       <fontAwesomeIcon :icon="icon" class="w-5" />
       {{ label }}
     </a>
   </li>
 </template>
+
+<style scoped>
+@reference "../style.css";
+
+.link-border-underline {
+  @apply border-1 border-transparent transition-colors duration-150 hover:border-main focus:border-1 focus:border-main focus:outline-0;
+  @apply after:absolute after:bottom-0 after:left-0 after:w-0 after:border-b-1 after:border-main after:duration-300 after:ease-out hover:after:w-full focus:after:w-full;
+}
+</style>
